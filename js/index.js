cbf720da9c04c17721e12b152b94798d6c76afb9
Kate <katelovejesus1110@gmail.com>
2021-08-04 19:26:24 +0800

    adjust hamburger toggle

--- js/index.js
+++ js/index.js
@@ -1,16 +1,7 @@
 //打開menu 選單
-    const menu=document.getElementById('menu-point')
-    const menuList=document.getElementById("menu-ul")
-    console.log('menu:',menu);
-    console.log('menuList:',menuList);
-    menu.addEventListener('click',openMenu)
+const menu=document.getElementById('menu-point')
+const menuList=document.getElementById("menu-ul")
 
-function openMenu(){
-    // menuList.style.display='block'
-    console.log("click style",menuList);
-    if(menuList.style.display ==='none'){
-        menuList.style.display ='block'
-    }else{
-        menuList.style.display ='none'
-    }
-}
+menu.addEventListener('click',function(){
+    menuList.classList.toggle('hide')
+})

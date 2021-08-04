7734a7c560918cc35947d9106eec0477184bb879
Kate <katelovejesus1110@gmail.com>
2021-08-04 17:19:27 +0800

    adjust the js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 //打開menu 選單
     const menu=document.getElementById('menu-point')
     const menuList=document.getElementById("menu-ul")
-    menu.addEventListener('click',openMenu())
-
+    console.log('menu:',menu);
+    console.log('menuList:',menuList);
+    menu.addEventListener('click',openMenu)
 
 function openMenu(){
     // menuList.style.display='block'
-    console.log("style",menuList.style.display);
-    if(menuList.style.display==='none'){
-        menuList.style.display='block'
+    console.log("click style",menuList);
+    if(menuList.style.display ==='none'){
+        menuList.style.display ='block'
     }else{
-        menuList.style.display='none'
+        menuList.style.display ='none'
     }
 }
